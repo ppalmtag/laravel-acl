c998792e0088708f6464a9b1bd29d291a92a5e19
Philipp Palmtag <palmtag@wd-plus.de>
2018-01-18 15:38:54 +0100

    Added relation and basic attributes

--- database/migrations/2018_01_18_141400_create_permission_user_table.php
+++ database/migrations/2018_01_18_141400_create_permission_user_table.php
@@ -15,6 +15,10 @@ class CreatePermissionUserTable extends Migration
     {
         Schema::create('permission_user', function (Blueprint $table) {
             $table->increments('id');
+            $table->integer('user_id')->unsigned();
+            $table->integer('permissions_id')->unsigned();
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->foreign('permissions_id')->references('id')->on('permissions');
             $table->timestamps();
         });
     }
--- database/migrations/2018_01_18_141500_create_permission_role_table.php
+++ database/migrations/2018_01_18_141500_create_permission_role_table.php
@@ -15,6 +15,10 @@ class CreatePermissionRoleTable extends Migration
     {
         Schema::create('permission_role', function (Blueprint $table) {
             $table->increments('id');
+            $table->integer('permissions_id')->unsigned();
+            $table->integer('role_id')->unsigned();
+            $table->foreign('permissions_id')->references('id')->on('permissions');
+            $table->foreign('role_id')->references('id')->on('roles');
             $table->timestamps();
         });
     }
