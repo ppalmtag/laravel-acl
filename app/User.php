0e69b4bd85221d978c8f4cee7b35a6f55e26d37d
Philipp Palmtag <palmtag@wd-plus.de>
2018-01-24 14:05:23 +0100

    Added functions to check for role and permission

--- app/User.php
+++ app/User.php
@@ -4,6 +4,7 @@ namespace App;
 
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Support\Collection;
 
 class User extends Authenticatable
 {
@@ -42,15 +43,68 @@ class User extends Authenticatable
     {
         return $this->belongsToMany('App\Permission');
     }
-    
-    
-    public function hasRole(Role $role): bool
+
+    /**
+     * Checks if the user has the given role. Accepts name of the role or a
+     * role object
+     *
+     * @param  Role | string  $role
+     * @return bool
+     */
+    public function hasRole($role): bool
     {
-        return $this->roles()->where('roles.id', $role->id)->exists();
+        $has_role = false;
+        if (is_string($role)) {
+            $has_role = $this->roles()->where('roles.name', 'like', $role)->exists();
+        } elseif ($role instanceof \App\Role) {
+            $has_role = $this->roles()->where('roles.id', $role->id)->exists();
+        }
+        return $has_role;
     }
-    
-    public function hasPermission(Permission $permission): bool
+
+    /**
+     * Checks if the user has the given permission
+     *
+     * @param  Permission | string  $permission
+     * @return bool
+     */
+    public function hasPermission($permission): bool
     {
-         return $this->permissions()->where('permissions.id', $permission->id)->exists();
+         return Permission::select('permissions.*')
+            ->join('permission_role', 'permission_role.permission_id', 'permissions.id')
+            ->join('roles', 'roles.id', 'permission_role.role_id')
+            ->join('role_user', 'role_user.role_id', 'roles.id')
+            ->where('role_user.user_id', $this->id)
+            ->when($permission instanceof Permission, function ($query) use ($permission) {
+                return $query->where('permissions.id', $permission->id);
+            }, function ($query) use ($permission) {
+                return $query->where('permissions.name', 'like', $permission);
+            })
+            ->union(
+                $this->permissions()
+                ->select('permissions.*')
+                ->when($permission instanceof Permission, function ($query) use ($permission) {
+                    return $query->where('permissions.id', $permission->id);
+                }, function ($query) use ($permission) {
+                    return $query->where('permissions.name', 'like', $permission);
+                })
+            )
+            ->exists();
+    }
+
+    /**
+     * Returns Permission objects of all the permissions of the user
+     *
+     * @return  Collection  of Permission objects
+     */
+    public function getPermissions(): Collection
+    {
+        return Permission::select('permissions.*')
+            ->join('permission_role', 'permission_role.permission_id', 'permissions.id')
+            ->join('roles', 'roles.id', 'permission_role.role_id')
+            ->join('role_user', 'role_user.role_id', 'roles.id')
+            ->where('role_user.user_id', $this->id)
+            ->union($this->permissions()->select('permissions.*'))
+            ->get();
     }
 }
